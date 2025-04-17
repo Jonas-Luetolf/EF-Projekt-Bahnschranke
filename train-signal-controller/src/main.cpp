d301c4ef0e1f39ce086bfddc1b9ba0b2b370e9c0
Nanoboter <Jean_Brawler@outlook.com>
2025-04-17 13:38:45 +0200

    Update main.cpp

--- train-signal-controller/src/main.cpp
+++ train-signal-controller/src/main.cpp
@@ -1,1 +1,20 @@
 #include <Arduino.h>
+
+#define SLAVE_ADDR 0x04
+#define SDA_PIN 0
+#define SCL_PIN 1
+
+
+void setup() {
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+
+}
+
+void loop() {
+    digitalWrite(4, HIGH);
+    delay(100);
+    digitalWrite(4, LOW);
+    delay(100);
+}
