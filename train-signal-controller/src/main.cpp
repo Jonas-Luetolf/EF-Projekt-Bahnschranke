84bd211831f97e46446845aa5633cfe0c2e2e8de
jonas <jonas.luetolf@outlook.com>
2025-05-08 08:42:41 +0200

    reformatting and clean up comments

--- train-signal-controller/src/main.cpp
+++ train-signal-controller/src/main.cpp
@@ -1,38 +1,40 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define SLAVE_ADDR 0x04
-#define SDA_PIN 0
-#define SCL_PIN 1
-#define RED_PIN 2
-#define YELLOW_PIN 3
-#define GREEN_PIN 4
+// I2C configs
+#define SLAVE_ADDR 0x05
+#define SDA_PIN 4
+#define SCL_PIN 5
+
+
+// Signal Pins
+#define RED_PIN 14
+#define YELLOW_PIN 15
+#define GREEN_PIN 26
 
 volatile uint8_t last_command_code = 0;
-
-
 
 
 void triggerRed() {
     digitalWrite(YELLOW_PIN, LOW);
     digitalWrite(GREEN_PIN, LOW);
     digitalWrite(RED_PIN, HIGH);
-    return;
 }
+
 
 void triggerYellow() {
     digitalWrite(RED_PIN, LOW);
     digitalWrite(GREEN_PIN, HIGH);
     digitalWrite(YELLOW_PIN, HIGH);
-    return;
 }
+
 
 void triggerGreen() {
     digitalWrite(RED_PIN, LOW);
     digitalWrite(YELLOW_PIN, LOW);
     digitalWrite(GREEN_PIN, HIGH);
-    return;
 }
+
 
 void onReceive(int numBytes){
     if (Wire.available()) {
@@ -40,7 +42,7 @@ void onReceive(int numBytes){
       Serial.print("INFO: got command ");
       Serial.println(last_command_code, HEX);
     }
-  }
+}
   
 
 void onRequest() {
@@ -52,28 +54,31 @@ void onRequest() {
     else {
       switch (last_command_code)
       {
-      // 0x01 close
+      // 0x01 Red: Halt
       case 0x01:
       Serial.print("INFO: executed command ");
       Serial.println(last_command_code, HEX);
+
       triggerRed();
-      // closeBarrier();
+      
       Wire.write(0);
       break;
     
-      // 0x02 open
+      // 0x02 Green + Yellow: freie Fahrt max. 40 km/h
       case 0x02:
         Serial.print("INFO: executed command ");
         Serial.println(last_command_code, HEX);
+
         triggerYellow();
-        //openBarrier();
+        triggerGreen();
+        
         Wire.write(0);
         break;
-
-      // check barrier state
+      
+      // 0x03 Green: freie Fahrt 
       case 0x03: {
         // Serial debug info executed command
-        Serial.print("INFO: executed command");
+        Serial.print("INFO: executed command ");
         Serial.println(last_command_code, HEX);
   
         triggerGreen();
@@ -87,34 +92,29 @@ void onRequest() {
       }
       last_command_code = 0;
     }
-  }
+}
 
-  void setup() {
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
+
+void setup() {
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(YELLOW_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
 
     Serial.begin(115200);
 
     Wire.setSDA(SDA_PIN);
     Wire.setSCL(SCL_PIN);
+
     Wire.begin(SLAVE_ADDR);
   
     Wire.onReceive(onReceive);
     Wire.onRequest(onRequest);
 
-
+    Serial.print("INFO: init I2C slave on Addr: ");
+    Serial.println(SLAVE_ADDR, HEX);
 }
+
 
 void loop() {
     delay(100);
-    /* 
-    triggerGreen();
-    delay(20);
-    triggerYellow();
-    delay(20);
-    triggerRed();
-    delay(20);
-
-    */
 }
